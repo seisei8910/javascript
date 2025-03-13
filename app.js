05363afbf22ab9fb851d824bd42859eb912c9a7f
SeiseiUtaka <185202718+seisei8910@users.noreply.github.com>
2025-03-13 22:33:08 +0000

    条件分岐

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
-// 文字列を結合
+// 条件分岐
 
-alert('Hello' + 'World');
+let orange = 100;
+let apple = 100;
 
-let str1 = 'Hello';
-let str2 = 'World!!';
-alert(str1 + str2);
+if(orange < apple){
+  alert('みかんの値段がりんごより安い');
+} else if(orange == apple){
+  alert('みかんとりんごが同じ値段');
+} else{
+  alert('みかんの値段がりんごより高い');
+}
