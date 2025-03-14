b99b63495ec078ae1e77ed2dc9e53f8d842f4392
SeiseiUtaka <185202718+seisei8910@users.noreply.github.com>
2025-03-14 23:42:24 +0000

    2章 JavaScriptの基本文法 確認問題

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-// for文
+// 確認問題
+// 足し算、引き算、掛け算、割り算の計算式と結果を、アラートウィンドウに表示
 
-let i;
-let num = 0;
+let num1 = 5;
+let num2 = 4;
+let add = num1 + num2;
+let sub = num1 - num2;
+let mul = num1 * num2;
+let div = num1 / num2;
 
-for(i = 1; i < 11; i++){
-  num = num + i;
-}
-
-alert('1から10までを足し算した結果は' + num + 'です');
+alert(num1 + 'と' + num2 + 'を足し算した結果は' + add + 'です');
+alert(num1 + 'を' + num2 + 'で引き算した結果は' + sub + 'です');
+alert(num1 + 'と' + num2 + 'を掛け算した結果は' + mul + 'です');
+alert(num1 + 'を' + num2 + 'で割り算した結果は' + div + 'です');
