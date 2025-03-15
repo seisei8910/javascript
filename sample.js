90c79a5df20f7f2ca8ce0c347e2b374cf79dd096
SeiseiUtaka <185202718+seisei8910@users.noreply.github.com>
2025-03-15 01:13:04 +0000

    変数の巻き上げ(ホイスティング)

--- sample.js
+++ sample.js
@@ -1,7 +1,12 @@
-// letのスコープ2
+// 変数の巻き上げ(ホイスティング)
 
-for (let i = 0; i < 10; i++) {
-  console.log(i)
+var str = "webcamp"
+
+function foo() {
+  console.log(str)
+  var str = "dmm webcamp"
+  console.log(str)
 }
 
-console.log(i)
+console.log(str)
+foo()
