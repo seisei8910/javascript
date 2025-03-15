a79162d2c9eca3191e4b98beebdd792db256146f
SeiseiUtaka <185202718+seisei8910@users.noreply.github.com>
2025-03-15 01:46:17 +0000

    簡単な関数の作成

--- app.js
+++ app.js
@@ -1,14 +1,6 @@
-// 確認問題
-// 足し算、引き算、掛け算、割り算の計算式と結果を、アラートウィンドウに表示
+// 関数
 
-let num1 = 5;
-let num2 = 4;
-let add = num1 + num2;
-let sub = num1 - num2;
-let mul = num1 * num2;
-let div = num1 / num2;
-
-alert(num1 + 'と' + num2 + 'を足し算した結果は' + add + 'です');
-alert(num1 + 'を' + num2 + 'で引き算した結果は' + sub + 'です');
-alert(num1 + 'と' + num2 + 'を掛け算した結果は' + mul + 'です');
-alert(num1 + 'を' + num2 + 'で割り算した結果は' + div + 'です');
+function addString(strA){
+  const addStr = "Hello " + strA;
+  return addStr;
+}
