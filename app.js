f6ada9ee70950f0c56f77daa1cad029f6eaa0866
SeiseiUtaka <185202718+seisei8910@users.noreply.github.com>
2025-03-15 01:51:38 +0000

    作成した関数を呼び出す

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
-// 関数
+// 作成した関数を呼び出す
+
+// メイン部分
+let alertString;
+
+// 作成した関数を呼び出し、変数へ格納
+alertString = addString("webCamp");
+
+// 変数の中身をアラートで表示する
+alert(alertString);
 
 function addString(strA){
   const addStr = "Hello " + strA;
