9603b74183a84f90c7a15807a1967d072138b2a8
SeiseiUtaka <185202718+seisei8910@users.noreply.github.com>
2025-03-15 00:49:01 +0000

    letのスコープ

--- sample.js
+++ sample.js
@@ -1,11 +1,12 @@
-// varのスコープ
-
-var str = "webcamp" //グローバルスコープ
+// letのスコープ
 
 function foo() {
-  console.log(str)
-  var y ="hello" //関数スコープ
+  let x = "webcamp"
+  {
+    let y = "hello webcamp"
+  }
+  console.log(x)
+  console.log(y)
 }
 
 foo()
-console.log(y)
